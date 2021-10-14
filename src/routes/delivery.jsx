8d33dd3d9ef01b9d493fbef270affbc1777e82f1
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-10-15 03:05:28 +0530

    added route links, icons, sidebar,several routes

--- src/routes/delivery.jsx
+++ src/routes/delivery.jsx
@@ -3,7 +3,7 @@ import "../index.css";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 
-const Dashboard = () => {
+const Delivery = () => {
   return (
     <div>
       <Header />
@@ -15,4 +15,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default Delivery;
