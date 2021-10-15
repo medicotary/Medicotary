4996b986500478a0b2a12492cee3fa073967b26d
Manas1820 <manasgupta1820@gmail.com>
2021-10-15 13:02:17 +0530

    Headers Improved

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,23 +6,18 @@ import bellicon from "../assets/bell.svg";
 class Header extends Component {
   render() {
     return (
-      <div className="w-full flex">
-        {/* top header */}
-        <div class="fixed w-full">
-          <div class="bg-white flex justify-between items-center px-6 py-2 w-full">
-            <img src={logo} alt="" class="object-scale-down h-14" />
-            <div class="flex justify-center items-center gap-4">
-              <h2 class="font-bold text-xl">Huma Medical Center</h2>
-              {/* bell icon */}
-              <img src={bellicon} alt="" class="h-6" />
-              {/* avatar */}
-              <img
-                src="https://pbs.twimg.com/profile_images/1378272639379144705/bOkqlvA6_400x400.jpg"
-                alt=""
-                class="ml-auto object-scale-down h-10 rounded-full"
-              />
-            </div>
-          </div>
+      <div class="fixed flex px-3 w-full border">
+        <img src={logo} alt="" className="mr-auto object-scale-down h-12"></img>
+        <div class="flex ml-auto items-center">
+          <h2 class="text-xl font-medium antialiased">Huma Medical Center</h2>
+          {/* bell icon */}
+          <img src={bellicon} alt="" class="h-6 px-4" />
+          {/* avatar */}
+          <img
+            src="https://pbs.twimg.com/profile_images/1378272639379144705/bOkqlvA6_400x400.jpg"
+            alt=""
+            class="object-scale-down h-10 rounded-full "
+          />
         </div>
       </div>
     );
