55ec1326112b7fb54949857f2af65d62489b0dec
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-10-20 12:51:30 +0530

    fixed sidebar navigation issue

--- src/routes/products.jsx
+++ src/routes/products.jsx
@@ -29,7 +29,7 @@ const Product = () => {
       <Header />
       {/* sidebar */}
       <div className="flex w-full overflow-x-hidden">
-        <Sidebar place="4" />
+        <Sidebar place="2" />
         {/* main content container */}
         <div className="p-6 bg-gray-50 mt-12 ml-72 w-full">
           <div className="flex flex-col">
