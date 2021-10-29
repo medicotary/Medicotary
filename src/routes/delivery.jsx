7f508e740fc295044ef9386721a4dcc892ca03d4
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-10-29 20:23:31 +0530

    made the delivery main page frontend

--- src/routes/delivery.jsx
+++ src/routes/delivery.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../index.css";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
+import DeliveryMainCard from "../components/delivery/maincard";
 
 const Delivery = () => {
   return (
@@ -10,7 +11,14 @@ const Delivery = () => {
       {/* sidebar */}
       <Sidebar place="5" />
       {/* main content container */}
-      <div></div>
+      <div className="mt-auto w-4/5 p-10 bg-gray-50 ml-auto">
+        <div className="flex flex-col">
+        <DeliveryMainCard />
+        <DeliveryMainCard />
+        <DeliveryMainCard />
+        <DeliveryMainCard />
+        </div>
+      </div>
     </div>
   );
 };
