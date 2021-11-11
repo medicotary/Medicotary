b5b49419aae8a5e0175db334f6ad3b15b5c29fbe
Manas1820 <manasgupta1820@gmail.com>
2021-11-11 20:40:26 +0530

    Auth Improved

--- src/redux/services/auth.service.js
+++ src/redux/services/auth.service.js
@@ -1,12 +1,27 @@
 import Axios from "axios";
 
-import { BASE_API_URL } from "./constants";
+import { BASE_API_URL } from "./constant";
+
+// const header = {
+//   "Content-Type":
+//     "application/x-www-form-urlencoded; charset=UTF-8;application/json",
+// };
 
 class AuthService {
   async login(data) {
     try {
-      const response = await Axios.post(`${BASE_API_URL}/auth/login`, data);
-      console.log(response.data);
+      const response = await Axios.post(`${BASE_API_URL}auth/login`, data);
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async signup(data) {
+    try {
+      const response = await Axios.post(`${BASE_API_URL}auth/signup`, data);
+      console.log(response);
       return response.data;
     } catch (error) {
       console.log(error);
