37265187ae45b141e0eca6775080aae6accd07b2
Manas1820 <manasgupta1820@gmail.com>
2021-11-11 20:32:31 +0530

    Added Storage

--- src/app.jsx
+++ src/app.jsx
@@ -15,29 +15,40 @@ import Faq from "./routes/faqs";
 import AddVendor from "./routes/addVendor";
 import AddProduct from "./routes/addProduct";
 import AddBill from "./routes/addBill";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux";
+const Loading = () => <div>Loading...</div>;
 const App = () => {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={Login}></Route>
-          <Route exact path="/forgot" component={ForgotPassword}></Route>
-          <Route exact path="/signup" component={Signup}></Route>
-          <Route exact path="/" component={Homepage}></Route>
-          <Route exact path="/dash" component={Dashboard}></Route>
-          <Route exact path="/products" component={Product}></Route>
-          <Route exact path="/billing" component={Billing}></Route>
-          <Route exact path="/vendors" component={Vendor}></Route>
-          <Route exact path="/delivery" component={Delivery}></Route>
-          <Route exact path="/profile" component={Profile}></Route>
-          <Route exact path="/notifications" component={Notifications}></Route>
-          <Route exact path="/faqs" component={Faq}></Route>
-          <Route exact path="/addvendor" component={AddVendor}></Route>
-          <Route exact path="/addproduct" component={AddProduct}></Route>
-          <Route exact path="/addbill" component={AddBill}></Route>
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <PersistGate loading={Loading} persistor={persistor}>
+          <Router>
+            <Switch>
+              <Route exact path="/login" component={Login}></Route>
+              <Route exact path="/forgot" component={ForgotPassword}></Route>
+              <Route exact path="/signup" component={Signup}></Route>
+              <Route exact path="/" component={Homepage}></Route>
+              <Route exact path="/dash" component={Dashboard}></Route>
+              <Route exact path="/products" component={Product}></Route>
+              <Route exact path="/billing" component={Billing}></Route>
+              <Route exact path="/vendors" component={Vendor}></Route>
+              <Route exact path="/delivery" component={Delivery}></Route>
+              <Route exact path="/profile" component={Profile}></Route>
+              <Route
+                exact
+                path="/notifications"
+                component={Notifications}
+              ></Route>
+              <Route exact path="/faqs" component={Faq}></Route>
+              <Route exact path="/addvendor" component={AddVendor}></Route>
+              <Route exact path="/addproduct" component={AddProduct}></Route>
+              <Route exact path="/addbill" component={AddBill}></Route>
+            </Switch>
+          </Router>
+        </PersistGate>
+      </Provider>
     </div>
   );
 };
