6a287670b35f305c7675c04b9c8fe648a1d9386a
Manas1820 <manasgupta1820@gmail.com>
2021-11-19 00:41:05 +0530

    Added Loader

---
+++ src/components/loader.jsx
@@ -0,0 +1,13 @@
+import Loader from "react-loader-spinner";
+import React, { Component } from "react";
+
+class Loading extends Component {
+  //other logic
+  render() {
+    return (
+      <Loader type="Bars" color={this.props.color} height={25} width={25} />
+    );
+  }
+}
+
+export default Loading;
