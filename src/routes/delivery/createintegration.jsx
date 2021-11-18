21888cd2b66835e39d253b5149391db04f8053e2
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-11-18 20:03:03 +0530

    added img name and des prop in delivery main card

---
+++ src/routes/delivery/createintegration.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../index.css";
+import Header from "../../components/header";
+import Sidebar from "../../components/sidebar";
+import DeliveryMainCard from "../../components/delivery/maincard";
+
+const Delivery = () => {
+  return (
+    <div>
+      <Header />
+      {/* sidebar */}
+      <Sidebar place="5" />
+      {/* main content container */}
+      <div className="mt-auto w-4/5 p-10 bg-gray-50 ml-auto">
+        <div className="flex flex-col">
+          {/* <DeliveryMainCard /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Delivery;
