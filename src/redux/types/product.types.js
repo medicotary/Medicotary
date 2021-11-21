2eb296c44f153fe4fae6412b6911fd855ac71d3b
Manas1820 <manasgupta1820@gmail.com>
2021-11-21 20:07:53 +0530

    Product Sate Management Improved

--- src/redux/types/product.types.js
+++ src/redux/types/product.types.js
@@ -1,6 +1,21 @@
+// To add a new product
 export const PRODUCT_ADD_REQUEST = "PRODUCT_ADD_REQUEST";
 export const PRODUCT_ADDED = "PRODUCT_ADDED";
-export const PRODUCT_ADD_ERROR = "PRODUCT_ADD_ERROR";
+
+//To set up edit request
+export const PRODUCT_EDIT_REQUEST = "PRODUCT_EDIT_REQUEST";
+export const PRODUCT_UPDATED = "PRODUCT_UPDATED";
+
+// To load all errors
+export const PRODUCT_ERROR = "PRODUCT_ERROR";
+
+// To Load all product
 export const PRODUCT_READ_REQUEST = "PRODUCT_READ_REQUEST";
 export const PRODUCT_READ = "PRODUCT_READ";
-export const PRODUCT_ERROR = "PRODUCT_ERROR";
+
+// // Loading Status
+// export const PRODUCT_LOAD = "PRODUCT_LOAD";
+
+// To add a new product
+export const PRODUCT_DELETE_REQUEST = "PRODUCT_DELETE_REQUEST";
+export const PRODUCT_DELETED = "PRODUCT_DELETED";
