db4536cb87577be27df230f9d97632b9d967e904
Manas1820 <manasgupta1820@gmail.com>
2021-11-30 03:06:07 +0530

    New Services Added

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,5 +2,6 @@
 import * as AuthActions from "./auth.action";
 import * as ProductActions from "./product.action";
 import * as VendorActions from "./vendor.actions";
+import * as HomeActions from "./home.action";
 // Export all actions here!
-export { AuthActions, ProductActions, VendorActions };
+export { AuthActions, ProductActions, VendorActions, HomeActions };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import { routerReducer } from "react-router-redux";
 import AuthReducer from "./auth.reducer";
 import VendorReducer from "./vendor.reducer";
 import ProductReducer from "./product.reducer";
+import HomeReducer from "./home.reducer";
 const allReducers = {
   // Put all of the reducers here!
   auth: AuthReducer,
   product: ProductReducer,
   vendor: VendorReducer,
   router: routerReducer,
+  home: HomeReducer
 };
 
 const rootReducer = combineReducers(allReducers);
