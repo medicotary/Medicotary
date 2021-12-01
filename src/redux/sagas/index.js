007ce79a18ec3533da809d884c370bddd5750b69
Manas1820 <manasgupta1820@gmail.com>
2021-12-01 17:41:59 +0530

    Updated New States

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -6,6 +6,7 @@ import authSaga from "./auth.saga";
 import productSaga from "./product.saga";
 import vendorSaga from "./vendor.sagas";
 import homeSaga from "./home.sagas";
+import billSaga from "./bill.sagas";
 // Connect types to sagas
 const rootSaga = function* root() {
   yield all([
@@ -14,6 +15,7 @@ const rootSaga = function* root() {
     fork(productSaga),
     fork(vendorSaga),
     fork(homeSaga),
+    fork(billSaga),
   ]);
 };
 
--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -4,5 +4,13 @@ import * as ProductTypes from "./product.types";
 import * as VendorTypes from "./vendor.types";
 import * as HomeTypes from "./home.types";
 import * as UserTypes from "./user.types";
+import * as BillTypes from "./bill.types";
 // Export all types here
-export { AuthTypes, ProductTypes, VendorTypes, HomeTypes, UserTypes };
+export {
+  AuthTypes,
+  ProductTypes,
+  VendorTypes,
+  HomeTypes,
+  UserTypes,
+  BillTypes,
+};
